129d95926938ed9d5f7ea710917e95ad56304742
yeonbong <yjune0107@gmail.com>
2022-10-24 14:26:28 +0900

    20221024 14:26

---
+++ movies/movies.typeDefs.js
@@ -0,0 +1,20 @@
+// The GraphQL schema
+export default `#graphql
+type Movie {
+  id: Int!
+  title: String!
+  year: Int!
+  genre: String
+  createdAt: String!
+  updatedAt: String!
+}
+  type Query {
+    movies: [Movie]
+    movie(id:Int!): Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id:Int!): Movie
+    updateMovie(id: Int! year: Int!): Movie
+  }
+`;
