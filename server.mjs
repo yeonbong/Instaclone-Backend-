b9465c3067662ea1c329a12047910a43618ce241
yeonbong <yjune0107@gmail.com>
2022-10-24 10:18:43 +0900

    prisma migrate

--- server.mjs
+++ server.mjs
@@ -3,15 +3,36 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 
 // The GraphQL schema
 const typeDefs = `#graphql
+type Movie {
+  id: Int
+  title: String
+  year: Int
+}
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!):Boolean
+    deleteMovie(title:String!): Boolean
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
